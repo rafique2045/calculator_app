2e5af5339292b2de51c4ed508f240ddef8665873
MD Rafique <143632705+rafique2045@users.noreply.github.com>
2025-01-15 02:07:57 +0600

    2

---
+++ lib/my_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final color;
+  final textColor;
+  final String buttonText;
+
+  const MyButton({
+    this.color,
+    this.textColor,
+    required this.buttonText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          color: color,
+          child: Center(
+            child: Text(
+              buttonText,
+              style: TextStyle(
+                color: textColor,
+                fontWeight: FontWeight.w700,
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
